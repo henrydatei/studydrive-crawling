dde407ce2e6bc49c9a49885914f0187669cc6e03
Henry Haustein <henrydatei@web.de>
2022-06-04 19:37:41 +0200

    typos

--- crawlAllCourses.py
+++ crawlAllCourses.py
@@ -5,7 +5,7 @@ import multiprocessing
 def produceOutput(universityID):
     print(universityID)
     totalResult = ""
-    for course in api.getUniversityCourses(universityID)[0:5]:
+    for course in api.getUniversityCourses(universityID):
         totalResult += produceOutput2(course) + "\n"
     return totalResult
         
@@ -31,11 +31,26 @@ def produceOutput2(course):
         spoName = ""
     return str(course["course_id"]) + ",\"" + course["number"] + "\",\"" + course["name"] + "\",\"" + ql + "\",\"" + ori + "\"," + str(course["active"]) + "," + str(course["copyright_warning"]) + "," + str(course["users_count"]) + "," + str(int(course["has_joined"])) + ",\"" + course["share_link"][:len(course["share_link"]) - 12] + "\",\"" + course["email_body"] + "\",\"" + course["email_subject"] + "\"," + str(course["university_id"]) + "," + str(int(moreData["is_sponsored"])) + ",\"" + moreData["image_url"] + "\",\"" + moreData["description_text"] + "\",\"" + moreData["course_description"]["exam_date"] + "\"," + spo + ",\"" + spoName + "\"," + currentDate
 
+try:
+    file = open("main-account.txt")
+    loginData = file.read()
+    mainUsername = loginData.split(":")[0]
+    mainPassword = loginData.split(":")[1]
+except:
+    print("main-account.txt not found")
+    print("I'll create the file main-account.txt with your credentials.")
+    file = open("main-account.txt", "w+")
+    mainUsername = input('Email: ')
+    mainPassword = input('Password: ')
+    file.write(str(mainUsername) + ":" + str(mainPassword))
+finally:
+    file.close()
+
 api = studydriveapi.StudydriveAPI()
-api.login("studydrive-crawl@web.de", "qwertzuiop")
+api.login(mainUsername, mainPassword)
 
 p = multiprocessing.Pool(20)
-unis = [university["university_id"] for university in api.getUniversities()[0:1]]
-with open("output_2.csv", "w") as f:
+unis = [university["university_id"] for university in api.getUniversities()]
+with open("output.csv", "w") as f:
     for result in p.imap(produceOutput, unis):
         f.write(result + "\n")
